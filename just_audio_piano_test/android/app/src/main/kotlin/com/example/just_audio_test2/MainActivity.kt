c4186c1d7441d1d2101842acc4deeec7db823579
JJonghyunLee <jaylee.jh7@gmail.com>
2024-02-02 12:11:07 -0700

    Add piano test app

---
+++ just_audio_piano_test/android/app/src/main/kotlin/com/example/just_audio_test2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.just_audio_test2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ just_audio_piano_test/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio_test2/pages/home.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
