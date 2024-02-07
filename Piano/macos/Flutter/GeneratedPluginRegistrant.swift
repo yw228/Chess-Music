b8b91ef2bd56d5bf576640130796d4d8d69e64be
JJonghyunLee <jaylee.jh7@gmail.com>
2024-02-07 12:16:36 -0700

    Add note playing to piano keys

--- Piano/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Piano/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
+import just_audio
 import path_provider_foundation
-import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
